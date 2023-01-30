da7a7686d02d31d9128e983d9dceea8292aca8e0
Ove Klinghed <ove.klinghed@gmail.com>
2023-01-30 21:04:34 +0100

    Add

---
+++ renew_install_lets_encrypt_cert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+certbotPemFile=/etc/letsencrypt/live/klinghed.se/fullchain.pem
+certbotKeyFile=/etc/letsencrypt/live/klinghed.se/privkey.pem
+nginxPemFile=/data/dockerVolumes/nginx-proxy/ssl/cert/star.klinghed.se.crt
+nginxKeyFile=/data/dockerVolumes/nginx-proxy/ssl/key/star.klinghed.se.key
+
+if certbot certonly --manual --preferred-challenges dns -d *.klinghed.se -d klinghed.se --force-renewal > /dev/null 2>&1; then
+   cp $certbotPemFile $nginxPemFile
+   cp $certbotKeyFile $nginxKeyFile
+fi   
